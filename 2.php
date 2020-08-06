8a83032a4f6a600cd6f63ed77db9066fb32012b8
Ray Bullard <hello@raybullard.com>
2020-08-06 12:46:33 -0700

    section 2 data types

--- 2.php
+++ 2.php
@@ -14,24 +14,40 @@
 		<article class="main-content col-xs-8">
 		
 
-<?php
+	<?php
 
 
 
-		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
+	/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
 
-		  Step 2: Add the two variables and display the sum with echo:
-
-
-		  Step3: Make 2 Arrays with the same values, one regular and the other associative
-
-		 
-			 */
-
-		
+	Step 2: Add the two variables and display the sum with echo:
 
 
-		?>
+	Step3: Make 2 Arrays with the same values, one regular and the other associative
+
+
+	*/
+
+	// Step 1
+	$number1 = 10;
+	$number2 = 20;
+	$result = $number1 + $number2;
+
+	// Step 2
+	echo "<h1>$result</h2>";
+	echo "<br/>";
+
+	// Step 3 
+	$array1 = array("Ray", "Jess", "Cooper", "Kaya");
+	$array2 = array("father" => 'Ray', "mother" => 'Jess', "son" => 'Cooper', "daughter" => 'Kaya' );
+
+	// Check to see if it works
+	print_r($array1);
+	echo "<br/>";
+	print_r($array2);
+	echo "<br/>";
+
+	?>
 
 	
 
